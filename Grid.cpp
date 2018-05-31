23ca85bc918edacd692f9779f56f9297ecb85fdb
Ben Euden <beneuden@hotmail.com>
2018-06-01 00:54:37 +0100

    some updates.

--- Grid.cpp
+++ Grid.cpp
@@ -11,6 +11,7 @@ void Grid::GenerateGrid(int sizeX, int sizeY)
 		for (int j = 0; j < sizeX; ++j)
 		{
 			Gems.push_back(std::make_shared<Gem>(Gem(i, j)));
+			Gems2[i][j]= std::make_shared<Gem>(Gem(i, j));
 		}
 
 	fieldSizeX = sizeX;
@@ -21,8 +22,15 @@ void Grid::DrawField(King::Engine &mEngine)
 {
 	for (GemIter g  = Gems.begin(); g != Gems.end(); ++g)
 	{
-		mEngine.Render(g->get()->Type, g->get()->posX, g->get()->posY);
+		//mEngine.Render(g->get()->Type, g->get()->posX, g->get()->posY);
+		//mEngine.Render(Gems2[get()->Type, g->get()->posX, g->get()->posY);
 	}
+
+	for (int i = 0; i < fieldSizeX; ++i)
+		for (int j = 0; j < fieldSizeY; ++j)
+		{
+			mEngine.Render(Gems2[i][j].get()->Type, Gems2[i][j].get()->posX, Gems2[i][j].get()->posY);
+		}
 }
 
 std::shared_ptr<Gem> Grid::gemAt(int row, int col)
@@ -54,6 +62,13 @@ bool Grid::isVerticalMatch(int row, int col)
 bool Grid::isMatch(int row, int col)
 {
 	return isHorizontalMatch(row, col) || isVerticalMatch(row, col);
+}
+
+void Grid::TmpSwap(int row1, int col1, int row2, int col2)
+{
+	Gem temp = *gemAt(row1, col1).get();
+	gemAt(row1, col1) = gemAt(row2, col2);
+	gemAt(row2, col2) = std::make_shared<Gem>(temp);
 }
 Grid::~Grid()
 {
--- Grid.h
+++ Grid.h
@@ -6,6 +6,7 @@
 
 typedef std::vector<std::shared_ptr<Gem>> GemVector;
 typedef GemVector::iterator GemIter;
+typedef std::vector<GemVector> DoubleGemVector;
 
 class Grid
 {
@@ -20,10 +21,55 @@ public:
 	bool isHorizontalMatch(int row, int col);
 	bool isVerticalMatch(int row, int col);
 
+
+	void GemSelect();
+	void GemDeselect();
+
+	// void showSuggestion
+
+	void TmpSwap(int row1, int col1, int row2, int col2);
 	std::shared_ptr<Gem> gemAt(int row, int col);
 
+	void swapGems(Gem& g1, Gem& g2, bool swapBack)
+	{
+		canPick = false;
+		King::Engine::Texture fromColor = g1.Type;
+		King::Engine::Texture toColor = g2.Type;
+
+		gameArray[getOrbRow(orb1)][getOrbCol(orb1)].orbColor = toColor;
+		gameArray[getOrbRow(orb1)][getOrbCol(orb1)].orbSprite = toSprite;
+		gameArray[getOrbRow(orb2)][getOrbCol(orb2)].orbColor = fromColor;
+		gameArray[getOrbRow(orb2)][getOrbCol(orb2)].orbSprite = fromSprite;
+		
+		
+		var orb1Tween = game.add.tween(gameArray[getOrbRow(orb1)][getOrbCol(orb1)].orbSprite).to({
+		x: getOrbCol(orb1) * orbSize + orbSize / 2,
+		   y : getOrbRow(orb1) * orbSize + orbSize / 2
+		}, swapSpeed, Phaser.Easing.Linear.None, true);
+		var orb2Tween = game.add.tween(gameArray[getOrbRow(orb2)][getOrbCol(orb2)].orbSprite).to({
+		x: getOrbCol(orb2) * orbSize + orbSize / 2,
+		   y : getOrbRow(orb2) * orbSize + orbSize / 2
+		}, swapSpeed, Phaser.Easing.Linear.None, true);
+		orb2Tween.onComplete.add(function() {
+			if (!matchInBoard() && swapBack) {
+				swapOrbs(orb1, orb2, false);
+			}
+			else {
+				if (matchInBoard()) {
+					handleMatches();
+				}
+				else {
+					canPick = true;
+					selectedOrb = null;
+				}
+			}
+		});
+	}
+
 	GemVector Gems;
+	DoubleGemVector Gems2;
 
 private:
 	int fieldSizeX, fieldSizeY;
+	bool canPick;
 };
